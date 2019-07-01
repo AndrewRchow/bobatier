11dcdb31a7c370e16af45b897ead0ac624786a4f
Andrew Chow <chowlick@gmail.com>
2019-07-01 10:23:34 -0700

    fix urls when using history.push

--- src/components/Partials/SignUp/index.js
+++ src/components/Partials/SignUp/index.js
@@ -47,7 +47,7 @@ class SignUpFormBase extends Component {
             })
             .then(() => {
                 this.setState({ ...INITIAL_STATE });
-                this.props.history.push(ROUTES.HOME);
+                this.props.history.push(process.env.PUBLIC_URL + ROUTES.HOME);
             })
             .catch(error => {
                 this.setState({ error });
